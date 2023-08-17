557a0b77a45784a66d89ff84737ab4d42bf79f08
David Andersson <lejonmanen@gmail.com>
2023-08-17 11:16:20 +0200

    ResRobot in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,64 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+/*
+Plan för livekodning:
+0. environment variables
+1. använd ResRobot API för att hämta information om en plats
+2. skriv interface som beskriver datan
+3. använda async med Geolocation
+*/
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Data {
+	stopLocationOrCoordLocation: StopLocation[]
 }
+interface StopLocation {
+	dist: number;   // distance from (lat,lon) in meters
+	extId: string;
+	name: string;
+}
+
+function App() {
+	// Innan vi fetchat är result NULL
+	// Sedan implementerar det Data-interfacet
+	const [result, setResult] = useState<Data | null>(null)
+
+	// console.log('Api key: ', import.meta.env.VITE_RESROBOT_API_KEY)
+	const handleClick = async () => {
+		const lat = 57.708895, lon = 11.973479
+		const url = `https://api.resrobot.se/v2.1/location.nearbystops?originCoordLat=${lat}&originCoordLong=${lon}&format=json&accessId=${import.meta.env.VITE_RESROBOT_API_KEY}`
+		const response = await fetch(url)
+		const data: Data = await response.json()
+		console.log('Data från ResRobot:', data);
+		setResult(data)
+	}
+
+	let stops: null | StopLocation[] = null
+	if( result ) {
+		stops = result.stopLocationOrCoordLocation
+	}
+
+	return (
+		<div>
+			<header> Demo </header>
+			<main>
+				<button onClick={handleClick}> Hämta information från ResRobot </button>
+				{stops 
+				? stops.map
+				: (<p> Här kommer hållplatser att visas... </p>)}
+			</main>
+		</div>
+	)
+}
+
+/*
+Higher order functions:
+- forEach - motsvarar en vanlig for-loop
+- find
+- filter
+- map
+
+for( let i=0; i<10; i++ )
+*/
 
 export default App
